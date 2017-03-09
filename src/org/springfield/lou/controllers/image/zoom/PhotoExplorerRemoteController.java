9c8e71b9c34480eda8ee84ed2ee5186472522169
Daniel Ockeloen <daniel@noterik.nl>
2017-03-09 16:03:42 +0100

    updated copyright message to match the license already included

--- src/org/springfield/lou/controllers/image/zoom/PhotoExplorerRemoteController.java
+++ src/org/springfield/lou/controllers/image/zoom/PhotoExplorerRemoteController.java
@@ -1,22 +1,21 @@
 /* 
-* PhotoExplorerRemoteController.java
 * 
 * Copyright (c) 2017 Noterik B.V.
 * 
-* This file is part of smt_mupopmobileapp, related to the Noterik Springfield project.
+* This file is part of MuPoP framework
 *
-* smt_mupopmobileapp is free software: you can redistribute it and/or modify
+* MuPoP framework is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
-* smt_mupopmobileapp is distributed in the hope that it will be useful,
+* MuPoP framework is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with smt_mupopmobileapp.  If not, see <http://www.gnu.org/licenses/>.
+* along with MuPoP framework .  If not, see <http://www.gnu.org/licenses/>.
 */
 package org.springfield.lou.controllers.image.zoom;
 
--- src/org/springfield/lou/controllers/intro/audio/AudioTestRemoteController.java
+++ src/org/springfield/lou/controllers/intro/audio/AudioTestRemoteController.java
@@ -1,23 +1,22 @@
 /* 
- * AudioTestRemoteController.java
- * 
- * Copyright (c) 2016 Noterik B.V.
- * 
- * This file is part of smt_mupopmobileapp, related to the Noterik Springfield project.
- *
- * smt_mupopmobileapp is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * smt_mupopmobileapp is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with smt_mupopmobileapp.  If not, see <http://www.gnu.org/licenses/>.
- */
+* 
+* Copyright (c) 2017 Noterik B.V.
+* 
+* This file is part of MuPoP framework
+*
+* MuPoP framework is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* MuPoP framework is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with MuPoP framework .  If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.springfield.lou.controllers.intro.audio;
 
 import org.json.simple.JSONObject;
